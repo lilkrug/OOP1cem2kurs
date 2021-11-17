5ea16371a37e2d2372404f37345f5f6d247e8e33
lilkrug <lilkrug_2003@mail.ru>
2021-11-17 16:22:27 +0300

    third commit

---
+++ 11 laba OOP/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+namespace _11_laba_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1 test1 = new Test1("qwerty");
+            Console.WriteLine(Reflector<Test1>.Assembly(test1.GetType().FullName));
+
+            Test2 test2 = new Test2("ward", 222, "IIS");
+            Console.WriteLine(Reflector<Test2>.IsPublicConstructors(test2.GetType().FullName));
+
+            var met = Reflector<Test2>.GetPublicMethods(test2.GetType().FullName);
+            foreach (string s in met)
+            {
+                Console.WriteLine(s);
+            }
+            var fields = Reflector<Test2>.GetFields(test2.GetType().FullName);
+            foreach (string s in fields)
+            {
+                Console.WriteLine(s);
+            }
+            var infaces = Reflector<Test2>.GetInterfaces(test2.GetType().FullName);
+
+
+            Reflector<Test1>.Invoke(test1, "Add");
+           
+
+            Test1 newTest1 = Reflector<Test1>.Create(typeof(Test1));
+        }
+
+   }
+       
+}
+
---
+++ 11 laba OOP/Reflector.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_laba_OOP
+{
+    public static class Reflector<T>
+    {
+        public static string Assembly(string ClassName)
+        {
+            return Type.GetType(ClassName).Assembly.FullName;
+        }
+        public static bool IsPublicConstructors(string ClassName)
+        {
+            if (Type.GetType(ClassName).GetConstructors().Length != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static IEnumerable<string> GetPublicMethods(string ClassName)
+        {
+            return Type.GetType(ClassName).GetMethods().Select(t => t.Name);
+        }
+        public static IEnumerable<string> GetFields(string ClassName)
+        {
+            return Type.GetType(ClassName).GetFields().Select(t => t.Name).Union(Type.GetType(ClassName).GetProperties().Select(t => t.Name));
+        }
+        public static IEnumerable<string> GetInterfaces(string ClassName)
+        {
+            return Type.GetType(ClassName).GetInterfaces().Select(n => n.FullName);
+        }
+        public static IEnumerable<string> GetMethodsParamType(string ClassName, Type paramtype)
+        {
+            return Type.GetType(ClassName).GetMethods().Where(a => (a.GetParameters().Where(b => b.ParameterType == paramtype)).Count() != 0).Select(p => p.Name);
+        }
+        internal static void Invoke(T t, string methodName)
+        {
+            Type type = t.GetType();
+            try
+            {
+
+                string[] _params = File.ReadAllLines(@"E:\Education\ООП 1 сем 2 курс\11 laba OOP\Reflection.txt");
+                string[] pr = { "asdfg" };
+
+                MethodInfo method = type.GetMethod(methodName);
+                Console.WriteLine("Результат выполнения метода : ");
+                Console.WriteLine(method.Invoke(t, _params));
+            }
+            catch (Exception er)
+            {
+                Console.WriteLine(er.Message);
+            }
+        }
+
+        internal static T Create(Type t)
+        {
+            T obj = (T)Activator.CreateInstance(t);
+            return obj;
+        }
+
+    } 
+}
